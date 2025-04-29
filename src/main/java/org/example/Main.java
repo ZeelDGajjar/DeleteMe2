912b4ccfe1d407f1e2ce8dc92f17f45f05a2f53c
zeelg <2474008@edu.vaniercollege.qc.ca>
2025-04-29 14:52:26 -0400

    Created selectionSort and added method doc

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,15 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int [] nums = {3, 1, 5, 2, 1, 4};
-        bubblesort(nums);    // {1, 1, 2, 3, 4, 5}
+        selectionSort(nums);    // {1, 1, 2, 3, 4, 5}
 
         System.out.println(Arrays.toString(nums));
-
     }
 
+    /**
+     * bubbleSort method with 0(n^2)
+     * @param nums the given int array
+     */
     public static void bubblesort(int[] nums) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i; j < nums.length - 1 - i; j++) {
@@ -21,5 +24,16 @@ public class Main {
                 }
             }
         }
+    }
+
+    /**
+     * SelectionSort idea:
+     * 315214
+     * 135214
+     * 125
+     * @param nums the given int array
+     */
+    public static void selectionSort(int[] nums) {
+
     }
 }
